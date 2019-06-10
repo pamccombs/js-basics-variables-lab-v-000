eb7ae19a7b404e98b024eedaf7ad823e44b47b7c
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-10 01:02:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 
-let mostProfitableNeighborhood = 'Chelsea';
+const mostProfitableNeighborhood = 'Chelsea';
 
-let companyCeo = 'Susan Smith';
+const companyCeo = 'Susan Smith';
